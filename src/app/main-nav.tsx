9409aace7708860be396422af84878f5c3bfe9cd
JagodaBodnar <jagodabodnar90@gmail.com>
2024-07-08 20:40:40 +0200

    create navigation

--- src/app/main-nav.tsx
+++ src/app/main-nav.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import logo from './assets/logo.png'
 
 const MainNav = () => {
-  const id = Math.round(Math.random()*100);
   return (
     <nav className="main-nav">
       <Link href="/"><img src={logo.src} alt="logo"/></Link>
